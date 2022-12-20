5e5cd46b560295e1a57e7bca092db56abdebcc6f
Jonatan <jsantanamay@gmail.com>
2022-12-20 14:38:38 -0600

    Agregando estilos y componente Testimonios

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Testimonio from './componentes/Testimonios.js';
 
 function App() {
   return (
     <div className="App">
+      <div className="contenedor-principal">
+        <h1>Esto es mi primera pagina React</h1>
+        <Testimonio />
+      </div>
     </div>
   );
 }
 
+
+//el unico que se va a expoertar de este archivo
 export default App;
+
+// la expoertacion nombrada exporta muchos
--- src/componentes/Testimonios.js
+++ src/componentes/Testimonios.js
@@ -6,6 +6,14 @@ function Testimonio(){
             <img className = "imagen-testimonio"
             src={require("../imagenes/testimonio-emma.png")}
             alt = "Foto de Emma"/>
+            <div className="contenedor-texto-testimonio">
+                <p className="nombre-testimonio">Jonatan Jafet</p>
+                <p className="cargo-testimonio">Ingeniero de software</p>
+                <p className="testo-tetimonio"> jeimircoimeociemrciemciermceirmceoicmeoirmcoeirmcirmceoircmeoircmeoic</p>
+            </div>
         </div>
     );
 }
+
+
+export default Testimonio;
