c841f0f8d3730fec19239662aaccd1448fd844fe
Jonatan <jsantanamay@gmail.com>
2022-12-21 13:24:43 -0600

    Comenzando segunda practica

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
 import './App.css';
-import Testimonio from './componentes/Testimonios.js';
+import Boton from './componentes/Boton';
+import freeCodeCampLogo from './imagenes/freecodecamp-logo.png'
 
 function App() {
+
+  const manejarClic = () =>{
+    console.log("Clic");
+  }
+
+  const reiniciarcontador = () =>{
+    console.log("Reinicio");
+  }
+
   return (
     <div className="App">
-      <div className="contenedor-principal">
-        <h1>Esto es mi primera pagina React</h1>
-        <Testimonio 
-          nombre = 'Maria Sanchez'
-          cargo = 'Ingenieria de Software'
-          testimonio = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse viverra quis metus nec dictum. Suspendisse feugiat justo sapien, ut efficitur quam tempor eget. Maecenas fermentum erat vel felis blandit mollis. Morbi malesuada purus eu tellus consectetur mattis. Etiam augue sem, imperdiet eu mollis id, condimentum nec lacus. In eleifend commodo diam, vel condimentum libero rhoncus eget. Vivamus semper justo eget elit feugiat varius. Cras tincidunt volutpat enim, et commodo sem rhoncus at. Suspendisse rhoncus blandit tortor vel accumsan. Aenean ut nibh vulputate, molestie est a, aliquet enim. Maecenas non nibh vel nibh dictum hendrerit eu sit amet augue. Donec varius orci quis tellus lacinia, accumsan mattis augue pharetra. Nulla mollis vitae orci et laoreet.'
-          imagen = 'emma'
+      <div className="logo-contenedor">
+        <img 
+        className='freecodecamp-logo'
+        src = {freeCodeCampLogo}
+        alt = 'logo de freeCodeCamp'
         />
-        <Testimonio 
-          nombre = 'Sarah Sanchez'
-          cargo = 'Ingenieria de Software'
-          testimonio = 'tur adipiscing elit. Suspendisse viverra quis metus nec dictum. Suspendisse feugiat justo sapien, ut efficitur quam tempor eget. Maecenas fermentum erat vel felis blandit mollis. Morbi malesuada purus eu tellus consectetur mattis. Etiam augue sem, imperdiet eu mollis id, condimentum nec lacus. In eleifend commodo diam, vel condimentum libero rhoncus eget. Vivamus semper justo eget elit feugiat varius. Cras tincidunt volutpat enim, et commodo sem rhoncus at. Suspendisse rhoncus blandit tortor vel accumsan. Aenean ut nibh vulputate, molestie est a, aliquet enim. Maecenas non nibh vel nibh dictum hendrerit eu sit amet augue. Donec varius orci quis tellus lacinia, accumsan mattis augue pharetra. Nulla mollis vitae orci et laoreet.'
-          imagen = 'sarah'
+      </div>
+      <div className='contenedor-principal'>
+        <Boton
+          texto = 'Click'
+          esBotonDeClic = {true}
+          manejarClic = {manejarClic}
+        />
+        <Boton
+          texto = 'Reiniciar'
+          esBotonDeClic = {false}
+          manejarClic = {reiniciarcontador}
         />
       </div>
     </div>
