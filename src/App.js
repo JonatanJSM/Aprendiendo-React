879b52ef55e27c6151f6b875f52fe06055502b69
Jonatan <jsantanamay@gmail.com>
2022-12-22 14:33:00 -0600

    Preparando proyecto

--- src/App.js
+++ src/App.js
@@ -1,44 +1,11 @@
 import './App.css';
-import Boton from './componentes/Boton';
-import freeCodeCampLogo from './imagenes/freecodecamp-logo.png'
-import Contador from "./componentes/Contador"
-import { useState } from 'react';
 
 function App() {
 
-    //Valor del estado, función para actualizar
-  const [numClics, sNumClics] = useState(0);
-
-  const manejarClic = () =>{
-    sNumClics(numClics + 1);
-  }
-
-  const reiniciarcontador = () =>{
-    sNumClics(0);
-  }
-
   return (
     <div className="App">
-      <div className="logo-contenedor">
-        <img 
-        className='freecodecamp-logo'
-        src = {freeCodeCampLogo}
-        alt = 'logo de freeCodeCamp'
-        />
-      </div>
-      <div className='contenedor-principal'>
-        <Contador numClics={numClics} />
-        <Boton
-          texto = 'Click'
-          esBotonDeClic = {true}
-          manejarClic = {manejarClic}
-        />
-        <Boton
-          texto = 'Reiniciar'
-          esBotonDeClic = {false}
-          manejarClic = {reiniciarcontador}
-        />
-      </div>
+      Aquí va lo de la página
+      Nos vamos a index, en index se va esta información
     </div>
   );
 }
