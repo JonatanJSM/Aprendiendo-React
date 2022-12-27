977cdc7e5922c09e8e4ed3fcce1c9c647e38dc27
Jonatan <jsantanamay@gmail.com>
2022-12-27 15:49:56 -0600

    Compoennete de clase y funcional

--- src/componentes/Testimonios.js
+++ src/componentes/Testimonios.js
@@ -1,22 +1,23 @@
 import React from "react";
 import '../hojas-de-estilos/Testimonio.css'
 
-function Testimonio(props){
-    return(
-        <div className = "contenedor-testimonio">
-            <img className = "imagen-testimonio"
-            src={require(`../imagenes/testimonio-${props.imagen}.png`)}
-            alt = "Foto de Emma" />
-            <div className="contenedor-texto-testimonio">
-                <p className="nombre-testimonio">
-                    <strong>{props.nombre}</strong>
-                </p>
-                <p className="cargo-testimonio">{props.cargo}</p>
-                <p className="testo-tetimonio">{props.testimonio}</p>
+class Testimonio extends React.Component{
+    render(){
+        return(
+            <div className = "contenedor-testimonio">
+                <img className = "imagen-testimonio"
+                src={require(`../imagenes/testimonio-${this.props.imagen}.png`)}
+                alt = "Foto de Emma" />
+                <div className="contenedor-texto-testimonio">
+                    <p className="nombre-testimonio">
+                        <strong>{this.props.nombre}</strong>
+                    </p>
+                    <p className="cargo-testimonio">{this.props.cargo}</p>
+                    <p className="testo-tetimonio">{this.props.testimonio}</p>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
-
 
 export default Testimonio;
